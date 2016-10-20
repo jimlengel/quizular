901b9f20b1618c4aab93cef6fb3563fca59543c8
Jim Lengel <jimlengel@gmail.com>
2016-10-20 12:15:29 -0700

    add controller for game

---
+++ quizular/app/controllers/games_controller.rb
@@ -0,0 +1,25 @@
+class GamesController < ApplicationController
+  def index
+    @games = Game.all
+  end
+
+  def show
+    @game = Game.find(params[:id])
+  end
+
+  def new
+  end
+
+  def create
+    @game = Game.new(game_params)
+   
+    @game.save
+    redirect_to @game
+  end
+
+  private
+    def game_params
+      params.require(:game).permit(:title, :question10)
+    end
+
+end
--- quizular/db/schema.rb
+++ quizular/db/schema.rb
@@ -10,13 +10,14 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20161020061450) do
+ActiveRecord::Schema.define(version: 20161020190740) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
   create_table "games", force: :cascade do |t|
     t.string   "title"
+    t.string   "question10"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
   end
