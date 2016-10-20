fd2235e15459870a497f4bd41c6049a3d91691cb
Jim Lengel <jimlengel@gmail.com>
2016-10-20 12:28:10 -0700

    add error handling for game form

--- quizular/app/controllers/games_controller.rb
+++ quizular/app/controllers/games_controller.rb
@@ -8,13 +8,17 @@ class GamesController < ApplicationController
   end
 
   def new
+    @game = Game.new
   end
 
   def create
     @game = Game.new(game_params)
    
-    @game.save
-    redirect_to @game
+    if @game.save
+      redirect_to @game
+    else
+      render 'new'
+    end
   end
 
   private
